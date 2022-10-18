6714a9dfab54b55d95bc8401d199cdb15fc8aafe
Bartosz Kwapisz <kwapisz.bartosz@gmail.com>
2022-10-18 13:44:42 +0200

    Create project based on Spring Initializr (https://start.spring.io/)

---
+++ src/main/java/pl/bkwapisz/taskprocessor/TaskProcessorApplication.java
@@ -0,0 +1,13 @@
+package pl.bkwapisz.taskprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bkwapisz/taskprocessor/TaskProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bkwapisz.taskprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
